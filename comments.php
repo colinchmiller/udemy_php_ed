045ccf20d1f811c35faeaeea14c0b8f4cc8347eb
colinmiller <colinchmiller@gmail.com>
2016-07-07 09:25:00 -0500

    comment lesson

---
+++ comments.php
@@ -0,0 +1,26 @@
+<!Doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+
+<body>
+
+  <?php
+
+// This is a comment: works on one line only
+
+/* This style will work on mulitple
+ multi-line
+ multi-line
+ multi-line
+ lines */
+
+  echo "Hello world";
+
+
+  ?>
+
+</body>
+</html>
