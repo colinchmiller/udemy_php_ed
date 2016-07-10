50fde60552cfc59e9d13dd8204e2d657618432e1
colinmiller <colinchmiller@gmail.com>
2016-07-10 10:07:12 -0500

    loops lessons

---
+++ for_loop.php
@@ -0,0 +1,22 @@
+<!Doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>For Loops</title>
+</head>
+
+<body>
+
+<?php
+
+for($i = 0; $i <= 5; $i++){
+
+  echo "The variable i is: " . $i . "<br>";
+
+}
+
+
+ ?>
+
+</body>
+</html>
---
+++ while_loop.php
@@ -0,0 +1,27 @@
+<!Doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>While Loops</title>
+</head>
+
+<body>
+
+<?php
+
+$x = 0;
+
+while($x < 5){
+
+echo "Variable x equals: " . $x . "<br>";
+
+$x ++;
+//also $x = $x + 1; or $x += 1;
+
+}
+
+
+ ?>
+
+</body>
+</html>
