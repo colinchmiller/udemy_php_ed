b6115cecbe963a564ba740413f007754bc4fd780
colinmiller <colinchmiller@gmail.com>
2016-07-10 10:12:40 -0500

    for each loops lesson

---
+++ for_each_loop.php
@@ -0,0 +1,24 @@
+<!Doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>For Each Loops</title>
+</head>
+
+<body>
+
+
+<?php
+// for each loop works only for arrays
+$numbers = array(1, 2, 3, 4, 5, 6);
+
+foreach($numbers as $number){
+
+echo $number . "<br>";
+
+}
+
+?>
+
+</body>
+</html>
