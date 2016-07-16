8595f791ac9fa63a963857482753a26a4e82a948
colinmiller <colinchmiller@gmail.com>
2016-07-16 10:30:33 -0500

    lesson on form validation

--- form.php
+++ form.php
@@ -3,12 +3,26 @@
 
 if(isset($_POST['submit'])) {
 
+  $names = array("Edwin", "Colin", "Peter", "Samid",
+   "Mohad", "Maria", "Jane", "Tom");
+  $minimum = 2;
+  $maximum = 15;
+
   $username = $_POST['username'];
   $password = $_POST['password'];
 
-  echo "Hello " . $username;
-  echo "<br>";
-  echo "Your password is: " . $password;
+  if(strlen($username) <= $minimum){
+    echo "Username is too short. Must be longer than ". $minimum . " characters.";
+  }
+  if(strlen($username) >= $maximum){
+    echo "Username is too long. Must be shorter than ". $maximum . " characters.";
+  }
+
+  if(!in_array($username, $names)){
+    echo "User does not exist.";
+  } else {
+    echo "Welcome";
+  }
 }
 
  ?>
