381a84c9822dbb31735c5039aec7b8f803a70089
colinmiller <colinchmiller@gmail.com>
2016-08-02 10:15:29 -0500

    post review

---
+++ the_post.php
@@ -0,0 +1,25 @@
+<?php
+
+echo $_POST['name'];
+
+ ?>
+
+<!Doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+
+<body>
+
+<form action="the_post.php" method="post">
+
+<input type="text" name="name">
+
+<input type="submit">
+
+</form>
+
+</body>
+</html>
