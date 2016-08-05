41b1a4c9b1cf4868ddc4729ee5a4f1c778d3d261
colinmiller <colinchmiller@gmail.com>
2016-08-05 09:08:09 -0500

    introduction to oop: creating and identifying a class

---
+++ class_car.php
@@ -0,0 +1,20 @@
+<?php
+
+//classes require capitalization to be recognized
+class Car {
+
+
+
+}
+
+if(class_exists("Car")){
+
+  echo "The class Car exists.";
+
+} else {
+
+  echo "The class Car does not exist";
+
+}
+
+ ?>
